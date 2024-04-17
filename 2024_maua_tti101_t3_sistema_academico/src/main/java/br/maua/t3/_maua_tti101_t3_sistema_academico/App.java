219bcedaad3784f99fd40e7413e14e6f1c4591ae
Tiago Tokugi de Albuquerque Massuda <tiagomassuda123@gmail.com>
2024-04-17 09:57:10 -0300

    aula de netbeans

---
+++ 2024_maua_tti101_t3_sistema_academico/src/main/java/br/maua/t3/_maua_tti101_t3_sistema_academico/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.maua.t3._maua_tti101_t3_sistema_academico;
+
+/**
+ *
+ * @author TiagoTokugi
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
