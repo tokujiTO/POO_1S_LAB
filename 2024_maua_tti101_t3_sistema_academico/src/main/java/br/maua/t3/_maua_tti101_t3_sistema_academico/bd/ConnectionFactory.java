0be0739b1301f68076de3e051d6a9b913dc0096c
Tiago Tokugi de Albuquerque Massuda <tiagomassuda123@gmail.com>
2024-04-22 09:14:10 -0300

    progrensso na conexão com o mysql

---
+++ 2024_maua_tti101_t3_sistema_academico/src/main/java/br/maua/t3/_maua_tti101_t3_sistema_academico/bd/ConnectionFactory.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.maua.t3._maua_tti101_t3_sistema_academico.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author TiagoTokugi
+ */
+public class ConnectionFactory {
+    private String host = "mysql-387re0124-tiagomassuda123-850a.b.aivencloud.com";
+    private String port = "23042";
+    private String db = "defaultdb";
+    private String user = "avnadmin";
+    private String password = "AVNS_Th71u61UU4O3AOhdubc";
+    
+    
+    public Connection obterConexão(){
+        try{
+            String stringConexao = String.format(
+                "jdbc:mysql://%s:%s/%s", host, port, db
+            );
+            var conexao = DriverManager.getConnection(
+                stringConexao, user, password
+            );
+            return conexao;
+        }
+        
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Deu erro filho");
+            return null;
+        }
+    
+    }
+    
+}
