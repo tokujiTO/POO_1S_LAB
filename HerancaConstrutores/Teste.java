f9bb01908e849f2eb84d43cb083d73279936bff5
Tiago Tokugi de Albuquerque Massuda <tiagomassuda123@gmail.com>
2024-05-15 10:58:05 -0300

    Aula de herancas com construtores e Heranças

---
+++ HerancaConstrutores/Teste.java
@@ -0,0 +1,8 @@
+public class Teste {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa("Ana", 19);
+        Aluno a = new Aluno("Pedro", 20);
+        Pessoa p2 = new Pessoa();
+        Aluno a2 = new Aluno();
+    }
+}
